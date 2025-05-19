c8151494855a9a0a2843996e952f681ecbcdd873
OrewaKimi <kimi93.com@gmail.com>
2025-05-19 11:09:51 +0700

    add crud, create function

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,13 +1,22 @@
-class Firestore {
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class FirestoreService {
   // get collection of note
+  final CollectionReference notes = FirebaseFirestore.instance.collection(
+    'notes',
+  );
 
   // CREATE: add a new note
+  Future<void> addNote(String note) {
+    return notes.add({
+      'note': note, 
+      'timestamp': Timestamp.now(),
+    });
+  }
 
   // READ: get notes from database
 
   // UPDATE: update notes given a doc id
 
   // DELETE: delete notes given a doc id
-
 }
