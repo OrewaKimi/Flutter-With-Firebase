7f35f9812953c454e7992103445bc55edd1e28c0
OrewaKimi <kimi93.com@gmail.com>
2025-05-19 11:29:15 +0700

    add button, firestore service, dialog, and text controller

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfirebase/services/firestore.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,12 +12,46 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Notes'),
-      ),
+      appBar: AppBar(title: Text('Notes')),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: openNoteBox,
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  // firestore service
+  final FirestoreService firestoreService = FirestoreService();
+
+  // text contoller
+  final TextEditingController textController = TextEditingController();
+
+  // open a dialog vox to add a note
+  void openNoteBox() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        // text user input
+        content: TextField(
+          controller: textController,
+          ),
+          actions: [
+            // button to save
+            ElevatedButton(
+              onPressed: () {
+                // add a new note
+                firestoreService.addNote(textController.text);
+
+                // clear the text controller
+                textController.clear();
+
+                // close the dialog
+                Navigator.pop(context);
+              },
+              child: Text('Save'),
+            ),
+          ],
+        ),
+      );
   }
 }
