452d31a9fd094ec6212da2eef4df48ebac78f437
OrewaKimi <kimi93.com@gmail.com>
2025-05-19 11:04:33 +0700

    add first commit

---
+++ lib/services/firestore.dart
@@ -0,0 +1,13 @@
+class Firestore {
+
+  // get collection of note
+
+  // CREATE: add a new note
+
+  // READ: get notes from database
+
+  // UPDATE: update notes given a doc id
+
+  // DELETE: delete notes given a doc id
+
+}
